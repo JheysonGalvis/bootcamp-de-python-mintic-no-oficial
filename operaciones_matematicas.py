608f10e6fd5bcf261757d666b47129798252bc78
Jheyson Galvis <tecnojheyson@gmail.com>
2024-07-14 12:47:23 -0500

    Avanzamos operaciones matemáticas y operadores de entrada y salida

---
+++ operaciones_matematicas.py
@@ -0,0 +1,27 @@
+#Operadores numéricos
+a = 10
+b = 3
+print(a > b)
+print(b < b)
+print(a >= b)
+print(a <=b)
+print(a == b)
+print(a != b) 
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicacion:", a * b)
+print("potenciacion:", a ** b)
+print("Division:", a / b)
+print("Modulo:", a % b)
+print("Division entera:", a // b)
+a += 2
+print(a)
+operacion_1 = 2 + 3 * 4
+operacion_2 = 2 + (3 * 4)
+print(operacion_1)
+print(operacion_2)
+operation_3 = (2 + 3) * (4 ** 2) / 8 - 1
+print(operation_3)
+
+
+
