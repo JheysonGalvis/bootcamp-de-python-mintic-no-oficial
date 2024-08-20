91e7e6378c297df62cf4667d13cef9cad77844b3
JheysonGalvis <tecnojheyson@gmail.com>
2024-08-20 17:30:05 -0500

    abordamos inputs

---
+++ inputs/input_numeros.py
@@ -0,0 +1,8 @@
+#pedirle un número al usuario
+numero = input("dame un número, campista: ")
+#Convierto el número a entero y lo multiplico por 2
+#resultado = int(numero) *2
+#Convierto el número a entero y lo multiplico por 2
+resultado = float(numero) *2
+#mostrando el resultado
+print(resultado)
---
+++ inputs/inputs.py
@@ -0,0 +1,5 @@
+#pedirle un dato al usuario
+nombre = input("¿Cual es tu nombre?")
+
+#mostrando el dato
+print(f'el nombre es: {nombre}') 
