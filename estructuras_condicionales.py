1e2c457c45222f2c0432cb825f202fd891cb8995
JheysonGalvis <tecnojheyson@gmail.com>
2024-08-20 17:05:48 -0500

    abordamos estructuras condicionales

---
+++ estructuras_condicionales.py
@@ -0,0 +1,10 @@
+is_member = True
+age = 11
+
+if is_member: 
+    if age >= 15:
+        print("puedes entrar") 
+    else:
+        print("no puedes entrar")
+else:
+    print("No eres miembro y NO TIENES ACCESO")
